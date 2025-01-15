9e1726c4121dbd2cb260673cc04e65c8bb14669d
KevinSteven <kperez@skgtecnologia.com>
2025-01-15 18:15:25 -0500

    cerrar sesion

--- BACKEND-NOVA/src/routes/User.py
+++ BACKEND-NOVA/src/routes/User.py
@@ -69,6 +69,6 @@ def login():
             'exp': datetime.utcnow() + timedelta(hours=1)
         }, SECRET_KEY, algorithm='HS256')
         
-        return jsonify({"message": "Login successful", "token": token}), 200
+        return jsonify({"message": "Login successful", "token": token, "userName": user.name, "userProject": user.project}), 200
     else:
         return jsonify({"message": "Invalid credentials"}), 401
